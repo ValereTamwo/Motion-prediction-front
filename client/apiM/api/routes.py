fd3cbb7b210534d3341040362edb93623810b9bf
fido15 <fidoknn@gmail.com>
2024-06-21 17:42:04 +0100

    ajout du point env

--- client/apiM/api/routes.py
+++ client/apiM/api/routes.py
@@ -1,35 +1,36 @@
 import os
 from flask import request, jsonify, send_file, current_app as app
+from dotenv import load_dotenv
 from .video_processing import process_video, load_dataset, store_predicted_frames, write_video, clean_repertoire
 from .utils import setup_logging
 from .predict import make_prediction
 
 setup_logging()
+load_dotenv()
 
 @app.route('/api/treat', methods=['POST'])
 def handle_request():
     video = request.files.get('video')  # Récupère la vidéo envoyée dans la requête
     print("Valeur de video:", video)
     if video:
-        home_dir = os.path.expanduser("~")
-        project_dir = os.path.join(home_dir, 'my_project_videos')
-        model=os.path.join(project_dir,'model3 (10).keras')
+        home_dir = os.path.expanduser(os.getenv("HOME_DIR"))
+        project_dir = os.path.join(home_dir, os.getenv("PROJECT_DIR"))
+        model = os.path.join(project_dir, os.getenv("MODEL"))
         os.makedirs(project_dir, exist_ok=True)
         video_path = os.path.join(project_dir, video.filename)
         video.save(video_path)  # Enregistre le fichier vidéo temporaire
-        output_folder = os.path.join(project_dir, 'pred')  # Spécifie le chemin complet où vous souhaitez enregistrer le fichier
-        output = os.path.join(project_dir, 'pred2')
-        final = os.path.join(project_dir, 'fin/processed_video.avi')
+        output_folder = os.path.join(project_dir, os.getenv("OUTPUT_FOLDER"))  # Spécifie le chemin complet où vous souhaitez enregistrer le fichier
+        output = os.path.join(project_dir, os.getenv("OUTPUT"))
+        final = os.path.join(project_dir, os.getenv("FINAL"))
         
         print('je fais le traitement de la video')
         path = process_video(video_path, output_folder)
-
 
         print('je charge les farmes de ma videos ')
 
         data = load_dataset(path)
         print('jeffectue ma predictions ')
-        predictions =make_prediction(model=model,data=data)
+        predictions = make_prediction(model=model, data=data)
         print('je stoke les frames de la predctions ')
         path2 = store_predicted_frames(data, output)
         print('je genere une videos avec les frames ')
